8688f7b8c055d77c14ce9cae7386d499b2046d6c
r1ddax <r1ddax00@gmail.com>
2021-05-01 20:24:08 -0300

    Function to write on the LCD, Datasheet and readme

--- src/main.cpp
+++ src/main.cpp
@@ -1,112 +1,82 @@
 #include <Arduino.h>
 
 /*
-  4-bit LCD pin:
-  (Just write, so pin RW grounded)
+|   4-bit LCD pin:
+|   (Just write, so pin RW grounded)
 */
 const int _data_pins[4]   =   { 0,   4,   5,   16 };  //  { db4, db5, db6, db7 }
 const int rs              =   12;                     //  Low: to send command | High: to send chars    
 const int en              =   14;                     //  Pulse to send bits
 
+/*
+|   Functions
+*/
+void initializeLCD(void);
 void pulseEn(void);
-void write4bits(uint8_t value);
-void sendBitsOf(uint8_t value, uint8_t _mode);
+void send4Bits(uint8_t value);
+void writeOnLCD(uint8_t value, uint8_t mode) ;
 
+/*
+============
+|   Setup
+============
+*/
 
 void setup() {
-  Serial.begin(9600); // Initializing Serial to monitorate some codes.
+  Serial.begin(9600); // Initializing Serial.
 
-
-  delay(5);
-  Serial.println ("...");
-  Serial.println (0x4C, BIN);
-  Serial.println (((0x4C)>>0) & 0x01);
-  Serial.println (((0x4C)>>1) & 0x01);
-  Serial.println (((0x4C)>>2) & 0x01);
-  Serial.println (((0x4C)>>3) & 0x01);
-
-  Serial.println (((0x4C)>>4) & 0x01);
-  Serial.println (((0x4C)>>5) & 0x01);
-  Serial.println (((0x4C)>>6) & 0x01);
-  Serial.println (((0x4C)>>7) & 0x01);
-
-  
-  // Initializing Pins Mode
-  pinMode(_data_pins[0], OUTPUT);
-  pinMode(_data_pins[1], OUTPUT);
-  pinMode(_data_pins[2], OUTPUT);
-  pinMode(_data_pins[3], OUTPUT);
-
-  pinMode(rs, OUTPUT);
-  pinMode(en, OUTPUT);
-  
-  digitalWrite(rs,    LOW); // Command mode
-  delayMicroseconds(100);   // Wait 40ms to energy the 16x2 (datasheet)
-  for (int i = 0; i < 3; i++) {
-    writeBits(0x03);          // Setting 4-bits mode, three times (datasheet).
-    delayMicroseconds(4500);  // Waiting 4500 µs → 4.5 ms
-  }
-
-  delayMicroseconds(150);
-  
-  // Set to 4-bit interface
-  writeBits(0x02);
+  initializeLCD();    // Functions to initialize the LCD, pins and commands (based HD44780 datasheet).
 
   // Clear Display
-  digitalWrite(rs,    LOW); // Function mode (low)
-  digitalWrite(_data_pins[0], 0);
-  digitalWrite(_data_pins[1], 0);
-  digitalWrite(_data_pins[2], 0);
-  digitalWrite(_data_pins[3], 0);
-  pulseEn();
-   
-  digitalWrite(rs,    LOW); // Function mode (low)
-  digitalWrite(_data_pins[0], 1);
-  digitalWrite(_data_pins[1], 0);
-  digitalWrite(_data_pins[2], 0);
-  digitalWrite(_data_pins[3], 0);
-  pulseEn();
+  writeOnLCD(0x01, LOW); // HIGH to send chars.
+                         // LOW to send command.
 
-  delay(1000); // 5 seconds to clear
-   
-  // Writing "L" on display with LOW/HIGH
-
-  digitalWrite(rs,    HIGH); // Write mode
-  digitalWrite(_data_pins[0], LOW);
-  digitalWrite(_data_pins[1], LOW);
-  digitalWrite(_data_pins[2], HIGH);
-  digitalWrite(_data_pins[3], LOW);
-  pulseEn();
-   
-  digitalWrite(rs,    HIGH); // Write mode
-  digitalWrite(_data_pins[0], LOW);
-  digitalWrite(_data_pins[1], LOW);
-  digitalWrite(_data_pins[2], HIGH);
-  digitalWrite(_data_pins[3], HIGH);
-  pulseEn();
-
-  // Writing on display "H" with Hex
-
-  digitalWrite(rs,    HIGH); // Write mode
-  digitalWrite(_data_pins[0], (((0x48)>>4) & 0x01));
-  digitalWrite(_data_pins[1], (((0x48)>>5) & 0x01));
-  digitalWrite(_data_pins[2], (((0x48)>>6) & 0x01));
-  digitalWrite(_data_pins[3], (((0x48)>>7) & 0x01));
-  pulseEn();
-   
-  digitalWrite(rs,    HIGH); // Write mode
-  digitalWrite(_data_pins[0], (((0x48)>>0) & 0x01));
-  digitalWrite(_data_pins[1], (((0x48)>>1) & 0x01));
-  digitalWrite(_data_pins[2], (((0x48)>>2) & 0x01));
-  digitalWrite(_data_pins[3], (((0x48)>>3) & 0x01));
-  pulseEn();
-
+  writeOnLCD(0x48, HIGH); // H, e, l, l, o, !
+  writeOnLCD(0x65, HIGH);
+  writeOnLCD(0x6C, HIGH);
+  writeOnLCD(0x6C, HIGH);
+  writeOnLCD(0x6F, HIGH);
+  writeOnLCD(0x21, HIGH);
 }
 
+/*
+============
+|   Loop
+============
+*/
 
 void loop() {
 
   
+}
+
+/*
+============
+| Functions 
+============
+*/
+
+void initializeLCD() {
+  // Initializing Pins Mode
+  for (int pin = 0; pin < 4; pin++) {
+    pinMode(_data_pins[pin], OUTPUT);
+  }
+
+  pinMode(rs, OUTPUT);
+  pinMode(en, OUTPUT);
+  digitalWrite(rs, LOW); // Command mode
+  
+  delayMicroseconds(100);   // Wait 40ms to energy the 16x2 (datasheet)
+  
+  // Trying 3x to set 4-bits mode:
+  for (int i = 0; i < 4; i++) {
+    send4Bits(0x03);          // Setting 4-bits mode, three times (datasheet).
+    delayMicroseconds(4500);  // Waiting 4500 µs → 4.5 ms
+  }
+
+  delayMicroseconds(150);
+  send4Bits(0x02);            // Set to 4-bit interface
+  delayMicroseconds(150);
 }
 
 void pulseEn() {
@@ -118,11 +88,48 @@ void pulseEn() {
   delayMicroseconds(100);
 }
 
-void writeBits(uint8_t value) {
+void send4Bits(uint8_t value) {
   // Wrinting the instructions with 4 bits:
   for (int i = 0; i < 4; i++) {
     digitalWrite(_data_pins[i], (value>>i) & 0x01); // Get value and turn an binary value
   }
   
   pulseEn();
+}
+
+void writeOnLCD(uint8_t value, uint8_t mode) {
+  delayMicroseconds(4500); // Delay to change rs mode without problems.
+
+  digitalWrite(rs, mode); /* Set rs in write mode (Write char     HIGH
+                                                   Send command   LOW)
+                          */
+
+  for (int h = 4; h < 8; h++) {
+    digitalWrite(_data_pins[h-4], (((value)>>h) & 0x01));
+  }
+  /*
+    Send the least significant bit of second part
+    of 4 in one byte, for exemple: (H / 0x48) → (0100) 1000
+    on pin, first send  [0] → 0
+                        [1] → 0
+                        [2] → 1
+                        [3] → 0
+  */
+             //   DB | 4 | 5 | 6 | 7 | 
+  pulseEn(); // Send | 0 | 1 | 0 | 0 |
+
+  for (int h = 0; h < 4; h++) {
+    digitalWrite(_data_pins[h], (((value)>>h) & 0x01));
+  }
+  /*
+    Send the least significant bit of first part
+    of 4 in one byte, for exemple: (H / 0x48) → 0100 (1000)
+    on pin, first send  [0] → 1
+                        [1] → 0
+                        [2] → 0
+                        [3] → 0
+  */
+
+             //   DB | 4 | 5 | 6 | 7 | 
+  pulseEn(); // Send | 1 | 0 | 0 | 0 |
 }
